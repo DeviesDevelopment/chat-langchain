f9bda669ccf21b5b0d77db71480b665714a22b5e
dvctokbok <drikvy.cappenberg@tocaboca.com>
2025-04-08 13:38:21 +0200

    fix grpc weaviate unavailable

--- backend/ingest.py
+++ backend/ingest.py
@@ -30,6 +30,7 @@ WEAVIATE_API_KEY = os.environ["WEAVIATE_API_KEY"]
 weaviate_client = weaviate.connect_to_weaviate_cloud(
             cluster_url=WEAVIATE_URL,
             auth_credentials=Auth.api_key(WEAVIATE_API_KEY),
+            skip_init_checks=True
         )
 
 
